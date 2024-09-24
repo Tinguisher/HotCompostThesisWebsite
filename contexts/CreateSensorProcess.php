ed096e0385bf6e5d31bae30244b06d1df775118a
unknown <knnthbinay@gmail.com>
2024-09-24 23:47:33 +0800

    separated with folder and can create values into database

---
+++ contexts/CreateSensorProcess.php
@@ -0,0 +1,48 @@
+<?php
+// if the file is accessed manually and not post
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    // make an error response
+    exit("Error. Not a POST request");
+}
+
+// get post values
+$moisturePercent = $_POST['inputMoisturePercent'];
+$temperatureCelsius = $_POST['inputTemperatureCelsius'];
+
+// check if there is empty input of data
+if ( empty($moisturePercent) || empty($temperatureCelsius)) {
+    // make an error response
+    exit("Error. All sensor values are required");
+};
+
+// access database
+$mysqli = require_once "./database.php";
+
+// try to create and catch if there is error
+try{
+    // make a string of sql
+    $sql = "INSERT INTO `sensor`(`hotCompost_id`, `moisturePercent`, `temperatureCelsius`) VALUES (1, ?, ?);";
+
+    // prepare the statement
+    $stmt = $mysqli -> prepare ($sql);
+
+    // bind the parameters to the statement
+    $stmt -> bind_param ('dd', $moisturePercent, $temperatureCelsius);
+
+    // execute the statement
+    $stmt -> execute();
+
+    // make a success response
+    echo "Successfully input the sensor values into the database.";
+}
+// if there is error in query
+catch (Exception $e){
+    // make an error signup response
+    echo "Error No: ". $e->getCode() ." - ". $e->getMessage();
+}
+
+// close statement and database
+$stmt -> close();
+$mysqli -> close();
+
+?>
