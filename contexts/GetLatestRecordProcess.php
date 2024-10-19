c04d2940a7232b627c2cdaf9f47033d1c2c3f5df
unknown <knnthbinay@gmail.com>
2024-10-19 19:15:01 +0800

    Done dashboard sensor reading

---
+++ contexts/GetLatestRecordProcess.php
@@ -0,0 +1,63 @@
+<?php
+// all contents to read as json
+header('Content-Type: application/json; charset=utf-8');
+
+// access database
+$mysqli = require_once "./database.php";
+
+// create a sql to get most recent reading of sensors
+$sql = "SELECT sensor.moisturePercent,
+        sensor.temperatureCelsius,
+        sensor.time
+    FROM `hotcompost`,
+        `sensor`
+    WHERE hotcompost.status NOT LIKE 'Completed'
+    ORDER BY sensor.time DESC
+    LIMIT 1";
+
+// try to get and catch if there is error
+try{
+    // prepare the statement
+    $stmt = $mysqli -> prepare ($sql);
+
+    // execute the statement
+    $stmt -> execute();
+
+    // get the result from the statement
+    $result = $stmt -> get_result();
+
+    // get one values from the database
+    $hotCompostReading = $result -> fetch_assoc();
+
+    // response message is dependent if there is hot compost in progress
+    $response = $hotCompostReading ? [
+        'status' => "success",
+        'message' => "Read",
+        'sensor' => $hotCompostReading
+    ] : [
+        'status' => "success",
+        'message' => "Create"
+    ];
+
+    // free data and close statement
+    $result -> free();
+    $stmt -> close();
+
+}
+
+// if there is error in query
+catch (Exception $e){
+    // make an error response
+    $response = [
+        'status' => "error",
+        'message' => "Error No: ". $e->getCode() ." - ". $e->getMessage()    // get error code and message
+    ];
+}
+
+// close the database
+$mysqli -> close();
+
+// return the response as json
+exit ( json_encode($response) );
+
+?>
---
+++ js/dashboard.js
@@ -0,0 +1,51 @@
+// Load js if HTML is done
+document.addEventListener('DOMContentLoaded', function () {
+    // get the status of current hot compost
+    fetch('../contexts/GetLatestRecordProcess.php')
+        // get response as json
+        .then(response => response.json())
+        // get objects from fetch
+        .then(data => {
+            // get the container id
+            const sensorContainer = document.getElementById("sensorContainer");
+            
+            // clear the content of the container
+            sensorContainer.innerHTML = "";
+
+            // if there is no current in progress, create
+            if (data.message == "Create") {
+                // create a message to inform the user
+                const createMessage = document.createElement('p');
+                createMessage.textContent = "There seems to be no in progress of hot compost, Create New One?";
+                sensorContainer.appendChild(createMessage);
+
+                // create a button to create a new compost
+                const createButton = document.createElement('button');
+                createButton.textContent = "Create New Compost";
+                sensorContainer.appendChild(createButton);
+
+                // go back to check new sensor reading
+                return;
+            }
+            // if there is compost in progress
+            // put the moisture reading inside the container
+            const moisturePercentage = document.createElement('p');
+            moisturePercentage.textContent = `Moisture Percentage: ${data.sensor.moisturePercent}`;
+            sensorContainer.appendChild(moisturePercentage);
+
+            // put the temperature reading inside the container
+            const temperatureCelsius = document.createElement('p');
+            temperatureCelsius.textContent = `Temperature: ${data.sensor.temperatureCelsius}`;
+            sensorContainer.appendChild(temperatureCelsius);
+
+            // put the time inside the container
+            const time = document.createElement('p');
+            time.textContent = `Time: ${data.sensor.time}`;
+            sensorContainer.appendChild(time);
+
+            return;
+        })
+
+        // error checker
+        .catch(error => console.error(error));
+});
