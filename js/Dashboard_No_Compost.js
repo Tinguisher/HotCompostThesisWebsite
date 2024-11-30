254a775b1a61de921532054926fbe3e9f926a685
Kenneth Binay <knnthbinay@gmail.com>
2024-12-01 00:57:16 +0800

    dashboard no compost getting every 1 second data done

--- js/Dashboard_No_Compost.js
+++ js/Dashboard_No_Compost.js
@@ -1,15 +1,25 @@
 // Load js if HTML is done
 document.addEventListener('DOMContentLoaded', function () {
-    // check if there is a compost that is in progress
-    fetch('../contexts/GetLatestRecordProcess.php')
-        // get response as json
-        .then(response => response.json())
-        // get objects from fetch
-        .then(data => {
-            // if there is a compost already, go to dashboard with reading
-            if (data.message == "Read") return (window.location.href = './dashboard.html');
-        })
+    // get latest record function to be looped
+    function getLatestRecord() {
+        // check if there is a compost that is in progress
+        fetch('../contexts/GetLatestRecordProcess.php')
+            // get response as json
+            .then(response => response.json())
+            // get objects from fetch
+            .then(data => {
+                // if there is a compost already, go to dashboard with reading
+                if (data.message == "Read") return (window.location.href = './dashboard.html');
+            })
 
-        // error checker
-        .catch(error => console.error(error));
+            // error checker
+            .catch(error => {
+                console.error(error);
+                // loop back to check new updates
+                setTimeout(getLatestRecord, 1000);
+            });
+    }
+
+    // go to get new record
+    getLatestRecord();
 });
