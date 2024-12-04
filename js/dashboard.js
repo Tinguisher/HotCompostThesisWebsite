9b18898ba13911fe7b47db04b3a43c187976ec0d
Kenneth Binay <knnthbinay@gmail.com>
2024-12-04 22:52:14 +0800

    popout done, design needed

--- js/dashboard.js
+++ js/dashboard.js
@@ -102,8 +102,50 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // process of creating each notifications
     createNotificationRows = (notifications) => {
+        // get the container for the notifications to be input and clear it
+        const notificationContainer = document.querySelector("[data-notification-container]");
+        notificationContainer.innerHTML = "";
 
+        // get and output the notifications
+        notifications.forEach(notification => {
+            // get the template for sensor and clone it
+            const notificationTemplate = document.querySelector("[data-notification-template]");
+            const row = notificationTemplate.content.cloneNode(true).children[0];
+
+            // get the template child that data can be inserted
+            const type = row.querySelector("[data-type]");
+            const time = row.querySelector("[data-time]");
+
+            // place the data got from the fetch  
+            type.textContent = notification.type;
+            time.textContent = `TIME: ${notification.time}`;
+
+            // put each made row inside container
+            notificationContainer.appendChild(row);
+        });
     }
+
+    // if there is click in notification, toggle
+    const notificationButton = document.getElementById("notificationButton");
+    notificationButton.addEventListener('click', () => {
+        // get the popout to be seen
+        const notificationPopOut = document.getElementById("notificationPopOut");
+
+        // get 2 types of hue for bell
+        const yellowBell = "sepia(100%) saturate(1000%) hue-rotate(0deg)";
+        const whiteBell = "unset";
+
+        // toggle for each click
+        if (notificationButton.style.filter == yellowBell) {
+            notificationButton.style.filter = whiteBell;
+            notificationPopOut.style.display = "none";
+        }
+        else {
+            notificationButton.style.filter = yellowBell;
+            notificationPopOut.style.display = "flex";
+            
+        }
+    })
 
     // go to get new record
     getLatestRecord();
